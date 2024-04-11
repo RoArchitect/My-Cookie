ebca314aa98920380c97709d9e5d49aefa0b0786
RoArchitect <145255543+RoArchitect@users.noreply.github.com>
2024-04-11 18:37:42 +0200

    Create ClickPower.lua

---
+++ src/ReplicatedStorage/Upgrades/ClickPower.lua
@@ -0,0 +1,8 @@
+return {
+	Name  = "Click Power";
+	Description = "Add onto your multiplier every time you click the cookie!";
+	
+	Price = 10;
+	PriceIncreasePerPurchase = 40;
+	PerPurchase = 0.4;
+}
