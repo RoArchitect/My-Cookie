a395c10e17b077d88a06ebf71d954b406acef52b
RoArchitect <145255543+RoArchitect@users.noreply.github.com>
2024-04-11 18:38:02 +0200

    Create CookieQuality.lua

---
+++ src/ReplicatedStorage/Upgrades/CookieQuality.lua
@@ -0,0 +1,8 @@
+return {
+	Name  = "Cookie Quality";
+	Description = "Your cookie gets tastier, and, well, you earn more points.";
+	
+	Price = 25;
+	PriceIncreasePerPurchase = 50;
+	PerPurchase = 1.5;
+}
